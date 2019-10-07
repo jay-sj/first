d8d77a2a939b71481c98ec5a831926112313e84a
严忆海 <yyh@qq.com>
2019-10-07 17:12:48 +0800

    严忆海

--- src/js/index1.js
+++ src/js/index1.js
@@ -69,6 +69,7 @@
                 }
             })//end object 'img'
         })//end object window
+        $(document).trigger("scroll");
     }())
     ; (function () {
         $(".sidebar-back").click(function () {
@@ -78,4 +79,19 @@
                 500);
             return false;
         });
+    }())
+    ;(function(){
+        var div1 = document.querySelectorAll(".main-r-list>div");
+        var div2 = document.querySelectorAll(".main-r-con>div");
+        for(var i=0;i<div1.length;i++){
+            div1[i].index = i;
+            div1[i].onmouseover = function(){
+                for(var j =0;j<div1.length;j++){
+                    div1[j].className="";
+                    div2[j].style.display="none";
+                }
+                this.className="active-1";
+                div2[this.index].style.display="block";
+            }
+        }
     }())
